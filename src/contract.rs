821d86f3123319bd15e1e3e2e43682739aaef5e9
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2023-08-21 15:51:27 +0700

    fix: deploy

--- src/contract.rs
+++ src/contract.rs
@@ -36,78 +36,79 @@ pub fn execute(
     msg: HandleMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        HandleMsg::Receive(msg) => execute_receive(deps, info, msg),
-        HandleMsg::Send { recipients } => execute_send(deps, recipients),
+        HandleMsg::Receive(msg) => execute::execute_receive(deps, info, msg),
+        HandleMsg::Send { recipients } => execute::execute_send(deps, recipients),
     }
 }
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute_receive(
-    deps: DepsMut,
-    info: MessageInfo,
-    wrapper: Cw20ReceiveMsg,
-) -> Result<Response, ContractError> {
-    let msg: ReceiveMsg = from_binary(&wrapper.msg)?;
-    let balance = Balance::Cw20(Cw20CoinVerified {
-        address: info.sender.clone(),
-        amount: wrapper.amount,
-    });
-    match msg {
-        ReceiveMsg::Send { recipients } => execute_send(deps, recipients),
-    }
-}
-
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute_send(
-    deps: DepsMut,
-    recipients: Vec<Recipient>,
-) -> Result<Response, ContractError> {
-    let messages = send_tokens(deps.api, recipients)?;
-    let attributes = vec![attr("action", "send")];
-    Ok(Response::default())
-}
-
-fn send_tokens(
-    api: &dyn Api,
-    recipients: Vec<Recipient>,
-) -> StdResult<Vec<CosmosMsg>> {
-    let mut msgs = vec![];
-
-    for recipient in recipients {
-        let native_balance = &recipient.clone().amount.native;
-        let mut native_msgs: Vec<CosmosMsg> = if native_balance.is_empty() {
-            vec![]
-        } else {
-            vec![BankMsg::Send {
-                to_address: recipient.clone().address.into(),
-                amount: native_balance.to_vec(),
-            }
-            .into()]
-        };
-
-        let cw20_balance = &recipient.amount.cw20;
-        let cw20_msgs: StdResult<Vec<_>> = cw20_balance
-            .iter()
-            .map(|c| {
-                let msg = Cw20ExecuteMsg::Transfer {
-                    recipient: recipient.clone().address.into(),
-                    amount: c.amount,
-                };
-                let exec = WasmMsg::Execute {
-                    contract_addr: c.clone().address.to_string(),
-                    msg: to_binary(&msg)?,
-                    funds: vec![],
-                };
-                Ok(exec.into())
-            })
-            .collect();
-
-        native_msgs.append(&mut cw20_msgs?);
-        msgs.append(&mut native_msgs);
+pub mod execute {
+    use super::*;
+    pub fn execute_receive(
+        deps: DepsMut,
+        info: MessageInfo,
+        wrapper: Cw20ReceiveMsg,
+    ) -> Result<Response, ContractError> {
+        let msg: ReceiveMsg = from_binary(&wrapper.msg)?;
+        let balance = Balance::Cw20(Cw20CoinVerified {
+            address: info.sender.clone(),
+            amount: wrapper.amount,
+        });
+        match msg {
+            ReceiveMsg::Send { recipients } => execute_send(deps, recipients),
+        }
     }
 
-    // assert balance
-    Ok(msgs)
+    pub fn execute_send(
+        deps: DepsMut,
+        recipients: Vec<Recipient>,
+    ) -> Result<Response, ContractError> {
+        let messages = send_tokens(deps.api, recipients)?;
+        let attributes = vec![attr("action", "send")];
+        Ok(Response::default())
+    }
+
+    fn send_tokens(
+        api: &dyn Api,
+        recipients: Vec<Recipient>,
+    ) -> StdResult<Vec<CosmosMsg>> {
+        let mut msgs = vec![];
+
+        for recipient in recipients {
+            let native_balance = &recipient.clone().amount.native;
+            let mut native_msgs: Vec<CosmosMsg> = if native_balance.is_empty() {
+                vec![]
+            } else {
+                vec![BankMsg::Send {
+                    to_address: recipient.clone().address.into(),
+                    amount: native_balance.to_vec(),
+                }
+                .into()]
+            };
+
+            let cw20_balance = &recipient.amount.cw20;
+            let cw20_msgs: StdResult<Vec<_>> = cw20_balance
+                .iter()
+                .map(|c| {
+                    let msg = Cw20ExecuteMsg::Transfer {
+                        recipient: recipient.clone().address.into(),
+                        amount: c.amount,
+                    };
+                    let exec = WasmMsg::Execute {
+                        contract_addr: c.clone().address.to_string(),
+                        msg: to_binary(&msg)?,
+                        funds: vec![],
+                    };
+                    Ok(exec.into())
+                })
+                .collect();
+
+            native_msgs.append(&mut cw20_msgs?);
+            msgs.append(&mut native_msgs);
+        }
+
+        // assert balance
+        Ok(msgs)
+    }
 }
 
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
